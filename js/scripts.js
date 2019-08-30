7fc809724c7de79a3dc0ff819054722b1408228c
Student <example@example.com>
2019-08-30 07:44:02 -0700

    add JS function direction

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,13 @@
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
+  $("form#enternum").submit(function(event) {
     event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
+    var myEntry = $("myEntry").val();
+    var matrix = [];
+    for (i=0;i<=myEntry;i++) {
+      matrix.push(i)
 
 
-  });
-});
-
-
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
-};
-// var leapYear = function(year) {
-//   return false;
-// };
+      if (matrix[i].toString().includes("3")) {
+        matrix.splice(i,1,"Im sorry, Dave. Im afraid I cant do that.")
+      } else if ()
+    }
