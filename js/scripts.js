5e9373ef58a22a9b2fbd0a8eac78232d70a5bbcb
Dominique Youmans <ddyoumans@icloud.com>
2019-09-03 01:23:11 -0700

    partial JS files, new images, new HTML forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
   $("form#enternum").submit(function(event) {
     event.preventDefault();
-    var myEntry = $("myEntry").val();
-    var matrix = [];
-    for (i=0;i<=myEntry;i++) {
-      matrix.push(i)
-
-
-      if (matrix[i].toString().includes("3")) {
-        matrix.splice(i,1,"Im sorry, Dave. Im afraid I cant do that.")
-      } else if ()
-    }
