e8bd95c94dd215fe5efa785213b1afdfc6be6ea7
Dominique Youmans <ddyoumans@icloud.com>
2019-09-03 03:16:42 -0700

    JS additions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
-  $("form#enternum").submit(function(event) {
-    event.preventDefault();
+function Deltron(beepName, beepNumber) {
+  var thisArray = [];
+  for (var i = 0; i < beepNumber + 1; i++) {
+    var beeps = i.toString();
+    if (i % 3 === 0) {
+      thisArray.push('Im sorry, ' + beepName + ', Im afraid I cant do that');
+    } else if (beeps.includes('3')) {
+      thisArray.push("Im sorry Dave, I'm afraid I cant do that.");
+    } else if (beeps.includes('2')) {
+      thisArray.push('BOOP!');
+    } else if (beeps.includes('1')) {
+      thisArray.push('BEEP!');
+    } else {
+      thisArray.push(i);
+    }
+  }
+    return thisArray;
+};
